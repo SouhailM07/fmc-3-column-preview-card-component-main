100560e721e151ab46e190b2af83e87c23b115b5
shadow <lightshadow416@gmail.com>
2024-02-03 19:33:17 +0100

    project purpose was fulfilled

--- src/components/MyCard/MyCard.tsx
+++ src/components/MyCard/MyCard.tsx
@@ -1,4 +1,29 @@
 import "./mycard.css";
-export default function MyCard() {
-  return <>MyCard</>;
+
+interface MyCard_type {
+  img: string;
+  color: string;
+  title: string;
+  txt: string;
+}
+
+export default function MyCard({ img, color, title, txt }: MyCard_type) {
+  return (
+    <>
+      <section
+        className={`bg-${color} max-w-[14rem] text-white px-[2rem] space-y-[3rem] py-[2rem] `}
+      >
+        <div className="space-y-[1rem]">
+          <img src={img} alt="logo" />
+          <h1 className="w700 text-[2rem] font-bold">{title}</h1>
+          <p className="text-[15px] w400 ">{txt}</p>
+        </div>
+        <button
+          className={`text-${color} duration-300 transition-all hover:bg-transparent border-2 border-white hover:text-white bg-white  px-[1.4rem] font-medium text-[15px] rounded-full py-[0.5rem]`}
+        >
+          Learn More
+        </button>
+      </section>
+    </>
+  );
 }
--- src/components/MyContainer/MyContainer.tsx
+++ src/components/MyContainer/MyContainer.tsx
@@ -1,6 +1,55 @@
 import "./mycontainer.css";
 // components
+import { MyCard } from "../../components";
+// assets
+import img1 from "/icon-sedans.svg";
+import img2 from "/icon-suvs.svg";
+import img3 from "/icon-luxury.svg";
 
 export default function MyContainer() {
-  return <>MyContainer</>;
+  interface Cars {
+    img: string;
+    color: string;
+    title: string;
+    txt: string;
+  }
+  const cars: Cars[] = [
+    {
+      img: img1,
+      color: "BrightOrange",
+      title: "SEDANS",
+      txt: " Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+    },
+    {
+      img: img2,
+      color: "DarkCyan",
+      title: "SUVs",
+      txt: "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+    },
+    {
+      img: img3,
+      color: "VeryDarkCyan",
+      title: "Luxury",
+      txt: "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+    },
+  ];
+  return (
+    <>
+      <main className="flex justify-center items-center min-h-screen">
+        <article className="grid grid-cols-3 max-md:grid-cols-1 ">
+          {cars.map((e, i) => {
+            return (
+              <MyCard
+                key={i}
+                img={e.img}
+                title={e.title}
+                txt={e.txt}
+                color={e.color}
+              />
+            );
+          })}
+        </article>
+      </main>
+    </>
+  );
 }
